79aea7e995b240486456d5f70154ab91f6f54228
Abel VARGA <rangeoshun@gmail.com>
2018-06-01 08:56:46 +0200

    Add notes what to port

--- index.js
+++ index.js
@@ -95,3 +95,40 @@ const header = (attrs = {}) => [
 const body = (attrs = {}) => [div, { id: 'body', ...attrs }, 'Nice app']
 
 const app = () => [div, { id: 'app', style: app_style }, header, body]
+
+/*
+function createTag ( jsonml ) {
+
+    var i = 0;
+
+    if (jsonml[i] instanceof _ARRAY) {
+
+        createInner(jsonml);
+        return;
+    }
+
+    var htmlFrag = '<' + jsonml[0].toLowerCase();
+    i++;
+
+    if (jsonml[i] instanceof _OBJECT && !(jsonml[i] instanceof _ARRAY)) {
+
+        htmlFrag += createAttributes(jsonml[i]);
+        i++;
+    }
+
+    htmlFrag += '>';
+    htmlArray.push(htmlFrag);
+
+    if (typeof jsonml[i] === _STRING_STRING) {
+
+        htmlArray.push(jsonml[i]);
+        i++;
+    }
+
+    if (i < jsonml.length) {
+        createInner(jsonml.splice(i, jsonml.length - i));
+    }
+
+    htmlArray.push('</' + jsonml[0].toLowerCase() + '>');
+}
+*/

f2409393232e809449859d7ba9e5c6c7a851919d
Abel VARGA <rangeoshun@gmail.com>
2018-06-01 08:51:09 +0200

    More functions. What else?

--- index.js
+++ index.js
@@ -1,39 +1,101 @@
 'use strict'
 
-const noop = () => null
+// Utils
 
-const is_def = subj => subj != undefined
+const undef = undefined
+
+const noop = () => undef
+
+const is_def = subj => subj != undef
 
 const add = (a, b) => a + b
 
-const str = subject => add(subject, '')
+const str = subj => add(subj, '')
+
+const sym = subj => Symbol.for(subj)
+
+const sym_to_str = sym => Symbol.keyFor(sym)
 
 const is_str = subj => typeof subj == 'string'
 
-const iff = (cond, then, other) => (cond ? then() : other())
+const is_arr = subj => subj instanceof Array
+
+const is_obj = subj => subj instanceof Object
+
+const iff = (cond, then = noop, other = noop) => (cond ? then() : other())
 
 const has_length = arr => !!arr.length
 
 const reduce = ([first, ...rest] = [], acc = [], fn = noop) =>
   iff(
     is_def(first) || has_length(rest),
-    () => reduce(rest, fn(first, acc), fn),
+    () => reduce(rest, fn(acc, first), fn),
     () => acc
   )
 
 const flow = (subj, ...fns) => reduce(fns, fn => fn(subj))
 
-const to_lower = subj => subj.toLowerCase()
+const to_lower = subj => str(subj).toLowerCase()
 
 const sanitize = subj => escape(subj)
 
 const transform_key = subj => flow(subj, to_lower, sanitize)
 
+const add_to = (arr, subj) => arr.push(subj)
+
+const keys_of = subj => Object.keys(subj)
+
+const get = (subj = {}, key) => subj[key]
+
 const str_attrs = attrs =>
-  reduce(attrs.keys, '', (key, res) =>
-    add(res, ` ${transform_key(str(key))}="${str(attrs[key])}"`)
+  reduce(keys_of(attrs), '', (acc, key) =>
+    add(acc, ` ${transform_key(str(key))}="${str(get(attrs, key))}"`)
   )
 
-const str_inner = jsonml => reduce(jsonml, '', (inner, acc) => null)
+const str_inner = html_arr => jsonml =>
+  reduce(jsonml, '', (inner, acc) =>
+    iff(
+      is_str(inner),
+      add_to(html_arr, inner),
+      iff(is_arr(inner), str_tag(inner))
+    )
+  )
 
-const jsnml_to_html_string = jsonml => null
+const start_tag = subj => add('<', transform_key(subj))
+
+const str_tag = html_arr => jsonml => {
+  let i = 0
+
+  iff(is_arr(get(jsonml, i)), str_inner(jsonml))
+}
+
+const jsnml_to_html_string = jsonml => undef
+
+const elem = (type, attrs, ...cont) => (attrs = {}, cont = undef) => [
+  type,
+  attrs,
+  cont
+]
+
+const div = sym('div')
+
+// App
+
+const app_style = {
+  display: 'flex'
+}
+
+const header_style = {
+  height: '50px',
+  flex_shrink: 0
+}
+
+const header = (attrs = {}) => [
+  div,
+  { id: 'head', style: header_style, ...attrs },
+  'Title'
+]
+
+const body = (attrs = {}) => [div, { id: 'body', ...attrs }, 'Nice app']
+
+const app = () => [div, { id: 'app', style: app_style }, header, body]

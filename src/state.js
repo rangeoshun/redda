d2a4f35b2328dea42c006236a7f681004ac5a08d
Abel Varga <avarga@hajtospincer.hu>
2018-06-09 12:46:00 +0200

    Add state half baked

--- src/state.js
+++ src/state.js
@@ -3,20 +3,19 @@
 import undef from './consts'
 import _ from './utils'
 
-const add = state => (frag = _.noop) =>
-  (state = { ...state, [_.sym(frag.name)]: frag() })
+const add = (state = {}, frag) => ({ ...state, [_.sym(frag.name)]: frag() })
 
 const connect = state => (elem = _.noop, ...frags) => (...args) =>
   elem.apply(null, [
-    ...args,
     _.reduce(frags, {}, (frag_state, { name }) => ({
       ...frag_state,
       [name]: state[_.sym(name)]
-    }))
+    })),
+    args
   ])
 
 const state = (state = {}) => ({
-  add: add(state),
+  add: frag => (state = add(state, frag)),
   connect: connect(state),
   get: () => state
 })
--- test/state.test.js
+++ test/state.test.js
@@ -2,16 +2,22 @@ import state from '../src/state'
 
 const state_get = state({ foo: 1 })
 
-test(state_get.get.name, () => expect(state_get.get()).toEqual({ foo: 1 }))
+test('state#get', () => expect(state_get.get()).toEqual({ foo: 1 }))
 
-const state_add = state({ foo: 1 })
-const frag = () => ({ bar: 1 })
+const state_add = state()
+const frag_1 = () => ({ bar: 1 })
+const frag_2 = () => ({ baz: 2 })
 
-state_add.add(frag)
+state_add.add(frag_1)
+state_add.add(frag_2)
 
-test(state_add.add.name, () =>
+test('state#add', () =>
   expect(state_add.get()).toEqual({
-    foo: 1,
-    [Symbol.for(frag.name)]: { bar: 1 }
-  })
-)
+    [Symbol.for(frag_1.name)]: { bar: 1 },
+    [Symbol.for(frag_2.name)]: { baz: 2 }
+  }))
+
+const state_conn = state({ foo: 1 })
+const mock_elem = (got_state, [attrs, ...cont]) => null
+
+// test('state#connect', () => expect())

dc3a125cb3a89ffb5e3ef8b1759a85fa64b7064b
Abel Varga <avarga@hajtospincer.hu>
2018-06-10 20:02:59 +0200

    Rename `core` to `render`

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@
 
 import consts from './consts'
 import dom from './dom_syms'
-import core from './core'
+import render from './core'
 import utils from './utils'
 import state from './state'
 
 export default {
   consts,
   dom,
-  core,
+  render,
   utils,
   state
 }

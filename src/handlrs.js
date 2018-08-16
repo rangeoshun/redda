48abf85d2bb4157b0f3d3e383b04c6c3bacc1e19
Abel VARGA <avarga@whitepages.com>
2018-08-16 11:25:54 +0200

    Add handlr funcs

--- src/handlrs.js
+++ src/handlrs.js
@@ -1,5 +1,29 @@
 'use strict'
 
-const handlrs = () => {}
+import { rnd_id } from './utils'
+
+const reg = (store, handlr) => {
+  handlr_id = rnd_id()
+
+  const new_store = {
+    ...store,
+    [handlr_id]: handlr
+  }
+  return [handlr_id, new_store]
+}
+
+const handlrs = store_ => {
+  store_ = {}
+
+  return {
+    reset: () => (store_ = {}),
+    reg: handlr => {
+      const [handlr_id, new_store] = reg(store, handlr)
+      store_ = new_store
+
+      return handlr_id
+    }
+  }
+}
 
 export default handlrs

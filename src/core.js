303cfaae5bc275e425a0ac4cfc6d5ad26b10552e
Abel VARGA <avarga@whitepages.com>
2018-08-16 13:46:35 +0200

    Pass handlrs in core

--- src/core.js
+++ src/core.js
@@ -23,53 +23,67 @@ export const str_style = attrs => {
 const is_style = key => key === 'style'
 const is_handl = key => key.match(/^on/)
 
-export const str_attrs = attrs => {
+export const str_attrs = (attrs, handlrs) => {
   if (!_.is_obj(attrs)) return ''
 
   return _.reduc(_.keys_of(attrs), '', (acc, key) => {
     const trans_key = _.transform_key(_.str(key))
     const conc = `${acc} ${trans_key}=`
+    const val = _.get(attrs, key)
 
-    if (is_style(trans_key)) return conc + `"${str_style(_.get(attrs, key))}"`
-    if (is_handl(trans_key)) return conc + `"redda.events[${foo}]"`
+    if (is_style(trans_key)) return conc + `"${str_style(val)}"`
 
-    return conc + `"${_.str(_.get(attrs, key))}"`
+    if (is_handl(trans_key)) {
+      const handlr_id = handlrs.reg(val)
+      return conc + `"redda.events[${handlr_id}]"`
+    }
+
+    return conc + `"${_.str(val)}"`
   })
 }
 
-export const str_inner = (jsonml, html_arr = []) =>
+export const str_inner = (jsonml, html_arr = [], handlrs) =>
   _.reduc(jsonml, html_arr, (acc, inner) => {
     if (_.is_str(inner)) return [...acc, inner]
-    if (_.is_arr(inner)) return build_html(inner, acc)
+    if (_.is_arr(inner)) return build_html(inner, acc, handlrs)
 
     return acc
   })
 
-export const open_tag = (type, attrs) =>
-  `<${_.transform_key(type)}${str_attrs(attrs)}>`
+export const open_tag = (type, attrs, handlrs) =>
+  `<${_.transform_key(type)}${str_attrs(attrs, handlrs)}>`
 
 export const close_tag = type => `</${_.transform_key(type)}>`
 
-export const wrap_tag = (first, second, ...rest) => {
+export const wrap_tag = (handlrs, first, second, ...rest) => {
   const inner = (!_.is_obj(second) && [second]) || []
 
   return [
-    open_tag(first, _.is_obj(second) && second),
+    open_tag(first, _.is_obj(second) && second, handlrs),
     ...str_inner([...inner, ...rest]),
     close_tag(first)
   ]
 }
 
-export const build_html = ([first, second, ...rest] = [], html = []) => {
-  if (_.is_arr(first)) return [...html, ...str_inner([first, second, ...rest])]
-  if (_.is_str(first)) return [...html, ...wrap_tag(first, second, ...rest)]
+export const build_html = (
+  [first, second, ...rest] = [],
+  html = [],
+  handlrs
+) => {
+  if (_.is_arr(first))
+    return [...html, ...str_inner([first, second, ...rest], [], handlrs)]
+
+  if (_.is_str(first))
+    return [...html, ...wrap_tag(handlrs, first, second, ...rest)]
+
   if (_.is_sym(first))
-    return [...html, ...wrap_tag(_.sym_to_str(first), second, ...rest)]
+    return [...html, ...wrap_tag(handlrs, _.sym_to_str(first), second, ...rest)]
 
   return html
 }
 
-export const to_html = jsonml => _.join(build_html(jsonml))
+export const to_html = (jsonml, handlrs) =>
+  _.join(build_html(jsonml, [], handlrs))
 
 export const to_jsonml = ([first, ...rest] = []) => {
   if (_.is_str(first) || _.is_sym(first) || _.is_obj(first))
@@ -88,7 +102,7 @@ export const elem = fn => (attrs, ...cont) => {
 }
 
 export default handlrs => (node, app) => {
-  const render = () => (node.innerHTML = to_html(to_jsonml(app)))
+  const render = () => (node.innerHTML = to_html(to_jsonml(app), handlrs))
 
   handlrs.reset()
   render()

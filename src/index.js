8c6813453b3fa179149b4db3d58563daf2f0e747
Abel VARGA <rangeoshun@gmail.com>
2018-08-18 09:11:55 +0200

    Clean up core

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@
 
 import consts from './consts'
 import dom from './dom_syms'
-import renderer from './core'
+import render_ from './core'
 import utils from './utils'
 import state from './state'
 import handlrs_ from './handlrs'
 
 const handlrs = handlrs_()
-const render = renderer(handlrs)
+const render = render_(handlrs)
 
 export default {
   consts,
